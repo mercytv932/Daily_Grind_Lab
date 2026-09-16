7155c99c823540c766ce72fcc5ee5c68c132a631
Yosef <mercytv932@gmail.com>
2026-09-16 14:49:29 -0400

    Finish with the server.js set up and app running

---
+++ server.js
@@ -0,0 +1,18 @@
+const path = require("path");
+const express = require("express");
+
+const app = express();
+
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+}); //Someone requests home page? send them home Page.
+
+app.get("/contact", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/contact.html"));
+}); //Someone requests contact page? send them contact page.
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+}); //Turn the server on or run it.
